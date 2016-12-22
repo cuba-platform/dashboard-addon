21da430317420568492b60734c8d0753cb5ee9f7
aleksey-stukalov <stukalov@haulmont.com>
2016-12-22 15:35:44 +0400

    Map widget

---
+++ modules/web/src/com/audimex/dashboard/web/frames/map/MapFrame.java
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) 2016-2016 Haulmont. All rights reserved.
+ */
+package com.audimex.dashboard.web.frames.map;
+
+import com.haulmont.cuba.gui.components.AbstractFrame;
+
+import java.util.Map;
+
+public class MapFrame extends AbstractFrame {
+    @Override
+    public void init(Map<String, Object> params) {
+        super.init(params);
+
+
+    }
+}
