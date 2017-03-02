954953582e7e36861a2a6fb610100616d6e57aac
Nikita Petunin <petunin@haulmont.com>
2017-03-02 17:58:23 +0400

    Remove redundant files

--- modules/web/src/com/audimex/dashboard/web/screens/DashboardScreen.java
+++
@@ -1,9 +0,0 @@
-/*
- * Copyright (c) 2016-2017 Haulmont. All rights reserved.
- */
-package com.audimex.dashboard.web.screens;
-
-import com.haulmont.cuba.gui.components.AbstractWindow;
-
-public class DashboardScreen extends AbstractWindow {
-}
