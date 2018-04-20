59d4343c52f21c21a0b980f7f2814dcd4d2a162c
Evgeniy Loginov <loginov@haulmont.com>
2018-04-20 14:44:13 +0400

    DASH-10 Create frame 'Palette'

---
+++ modules/web/src/com/audimex/dashboard/web/dashboard/components/PaletteButton.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright (c) 2016-2018 Haulmont. All rights reserved.
+ */
+
+package com.audimex.dashboard.web.dashboard.components;
+
+import com.audimex.dashboard.entity.Widget;
+import com.audimex.dashboard.visual_model.LayoutType;
+import com.vaadin.server.Resource;
+import com.vaadin.ui.Button;
+
+public class PaletteButton extends Button {
+
+    protected LayoutType layoutType;
+    protected Widget widget;
+
+    public PaletteButton(String caption, Resource icon) {
+        super(caption, icon);
+        this.setWidth("100%");
+        this.setHeight("50px");
+    }
+
+    public LayoutType getLayoutType() {
+        return layoutType;
+    }
+
+    public void setLayoutType(LayoutType layoutType) {
+        this.layoutType = layoutType;
+    }
+
+    public Widget getWidget() {
+        return widget;
+    }
+
+    public void setWidget(Widget widget) {
+        this.widget = widget;
+    }
+}
---
+++ modules/web/src/com/audimex/dashboard/web/dashboard/components/WidgetsPaletteFrame.java
@@ -0,0 +1,66 @@
+/*
+ * Copyright (c) 2016-2018 Haulmont. All rights reserved.
+ */
+
+package com.audimex.dashboard.web.dashboard.components;
+
+import com.audimex.dashboard.entity.Widget;
+import com.haulmont.cuba.gui.components.AbstractFrame;
+import com.haulmont.cuba.gui.components.VBoxLayout;
+import com.haulmont.cuba.web.gui.components.WebComponentsHelper;
+import com.haulmont.cuba.web.gui.icons.IconResolver;
+import com.vaadin.ui.Layout;
+import fi.jasoft.dragdroplayouts.DDVerticalLayout;
+import fi.jasoft.dragdroplayouts.client.ui.LayoutDragMode;
+import fi.jasoft.dragdroplayouts.drophandlers.DefaultVerticalLayoutDropHandler;
+
+import javax.inject.Inject;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class WidgetsPaletteFrame extends AbstractFrame {
+    public static final String WIDGETS_PARAM = "WIDGETS_PARAM";
+
+    @Inject
+    protected VBoxLayout widgetsContainer;
+    @Inject
+    protected IconResolver iconResolver;
+
+    protected DDVerticalLayout ddVerticalLayout;
+
+    protected List<Widget> widgets;
+
+    @Override
+    public void init(Map<String, Object> params) {
+        super.init(params);
+        setParams(params);
+        configDDLayout();
+
+        ((Layout) WebComponentsHelper.unwrap(widgetsContainer)).addComponent(ddVerticalLayout);
+    }
+
+    protected void setParams(Map<String, Object> params) {
+        widgets = (List<Widget>) params.getOrDefault(WIDGETS_PARAM, new ArrayList<Widget>());
+    }
+
+    protected void configDDLayout() {
+        ddVerticalLayout = new DDVerticalLayout();
+        ddVerticalLayout.setDragMode(LayoutDragMode.CLONE);
+        ddVerticalLayout.setDropHandler(new DefaultVerticalLayoutDropHandler());
+
+        for (Widget widget : widgets) {
+            String caption = widget.getCaption() != null ? widget.getCaption() : "";
+            String icon = widget.getIcon() != null ? widget.getIcon() : "";
+
+            PaletteButton paletteButton = new PaletteButton(
+                    messages.getMainMessage(caption),
+                    iconResolver.getIconResource(icon)
+            );
+            paletteButton.setWidget(widget);
+            ddVerticalLayout.addComponent(paletteButton);
+        }
+    }
+
+
+}
