a4525ec3301dd4b7b89a61cfd0d98985397a42dd
Evgeniy Loginov <loginov@haulmont.com>
2018-04-20 15:38:54 +0400

    DASH-18 create initial structure for widget screen

---
+++ modules/web/src/com/audimex/dashboard/web/widget/WidgetBrowse.java
@@ -0,0 +1,9 @@
+/*
+ * Copyright (c) 2016-2018 Haulmont. All rights reserved.
+ */
+package com.audimex.dashboard.web.widget;
+
+import com.haulmont.cuba.gui.components.AbstractLookup;
+
+public class WidgetBrowse extends AbstractLookup {
+}
---
+++ modules/web/src/com/audimex/dashboard/web/widget/WidgetEdit.java
@@ -0,0 +1,10 @@
+/*
+ * Copyright (c) 2016-2018 Haulmont. All rights reserved.
+ */
+package com.audimex.dashboard.web.widget;
+
+import com.haulmont.cuba.gui.components.AbstractEditor;
+import com.audimex.dashboard.entity.Widget;
+
+public class WidgetEdit extends AbstractEditor<Widget> {
+}
