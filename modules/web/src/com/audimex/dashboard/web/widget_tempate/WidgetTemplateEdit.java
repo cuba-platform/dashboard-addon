9914912a21082dcc355b0a4f9ede4ad3622deb87
Evgeniy Loginov <loginov@haulmont.com>
2018-04-28 14:49:22 +0400

    DASH-50 added custom field 'frameId'

--- modules/web/src/com/audimex/dashboard/web/widget_tempate/WidgetTemplateEdit.java
+++ modules/web/src/com/audimex/dashboard/web/widget_tempate/WidgetTemplateEdit.java
@@ -8,11 +8,14 @@ import com.audimex.dashboard.model.Widget;
 import com.audimex.dashboard.web.parameter.ParameterBrowse;
 import com.haulmont.bali.util.ParamsMap;
 import com.haulmont.cuba.gui.components.AbstractEditor;
+import com.haulmont.cuba.gui.components.FieldGroup;
+import com.haulmont.cuba.gui.components.LookupField;
 import com.haulmont.cuba.gui.data.Datasource;
+import com.haulmont.cuba.gui.xml.layout.ComponentsFactory;
 
 import javax.inject.Inject;
+import java.util.Collections;
 import java.util.List;
-import java.util.Map;
 
 import static com.audimex.dashboard.web.parameter.ParameterBrowse.PARAMETERS;
 
@@ -20,15 +23,18 @@ public class WidgetTemplateEdit extends AbstractEditor<Widget> {
     @Inject
     protected Datasource<Widget> widgetDs;
     @Inject
+    protected FieldGroup fieldGroup;
+    @Inject
     protected ParameterBrowse paramsFrame;
+    @Inject
+    protected ComponentsFactory componentsFactory;
 
-    @Override
-    public void init(Map<String, Object> params) {
-        super.init(params);
-    }
+    protected LookupField frameIdLookup;
 
     @Override
     protected void postInit() {
+        initFrameIdField();
+
         paramsFrame.init(ParamsMap.of(
                 PARAMETERS,
                 getItem().getParameters()
@@ -41,5 +47,17 @@ public class WidgetTemplateEdit extends AbstractEditor<Widget> {
         widgetDs.getItem().setParameters(parameters);
 
         return super.preCommit();
+    }
+
+    protected void initFrameIdField() {
+        FieldGroup.FieldConfig frameIdConfig = fieldGroup.getField("frameId");
+        frameIdLookup = componentsFactory.createComponent(LookupField.class);
+        frameIdLookup.setDatasource(widgetDs, frameIdConfig.getProperty());
+        frameIdLookup.setOptionsList(getFrameIds());
+        frameIdConfig.setComponent(frameIdLookup);
+    }
+
+    protected List getFrameIds() {
+        return Collections.emptyList();
     }
 }
