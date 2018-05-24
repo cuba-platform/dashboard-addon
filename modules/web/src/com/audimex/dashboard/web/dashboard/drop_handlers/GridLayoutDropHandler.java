a7b627853e03d807c2c2a8b1d3a75f7e1a17668e
Evgeniy Loginov <loginov@haulmont.com>
2018-05-24 11:21:33 +0400

    added grid drophandler

--- modules/web/src/com/audimex/dashboard/web/dashboard/drop_handlers/GridLayoutDropHandler.java
+++ modules/web/src/com/audimex/dashboard/web/dashboard/drop_handlers/GridLayoutDropHandler.java
@@ -4,8 +4,62 @@
 
 package com.audimex.dashboard.web.dashboard.drop_handlers;
 
+import com.audimex.dashboard.web.dashboard.tools.DropLayoutTools;
+import com.audimex.dashboard.web.dashboard.vaadin_components.Draggable;
+import com.vaadin.event.dd.DragAndDropEvent;
+import com.vaadin.ui.Component;
+import fi.jasoft.dragdroplayouts.DDGridLayout;
 import fi.jasoft.dragdroplayouts.drophandlers.DefaultGridLayoutDropHandler;
+import fi.jasoft.dragdroplayouts.events.LayoutBoundTransferable;
 
 public class GridLayoutDropHandler extends DefaultGridLayoutDropHandler {
+    protected DropLayoutTools tools;
+
+    public GridLayoutDropHandler(DropLayoutTools tools) {
+        this.tools = tools;
+    }
+
+    @Override
+    protected void handleDropFromLayout(DragAndDropEvent event) {
+        LayoutBoundTransferable transferable = (LayoutBoundTransferable) event
+                .getTransferable();
+        DDGridLayout.GridLayoutTargetDetails details = (DDGridLayout.GridLayoutTargetDetails) event
+                .getTargetDetails();
+        DDGridLayout layout = (DDGridLayout) details.getTarget();
+        Component source = event.getTransferable().getSourceComponent();
+        Component comp = transferable.getComponent();
+
+        Component parent = layout.getParent();
+        while (parent != null) {
+            if (parent == comp) {
+                return;
+            }
+            parent = parent.getParent();
+        }
+
+        if (comp instanceof Draggable) {
+            int row = details.getOverRow();
+            int column = details.getOverColumn();
+
+
+            // If no components exist in the grid, then just add the
+            // component
+            if (!layout.getComponentIterator().hasNext()) {
+                layout.addComponent(comp, column, row);
+                return;
+            }
+
+            // If component was dropped on top of another component, abort
+            if (layout.getComponent(column, row) != null) {
+                return;
+            }
+
+            // Add the component
+
+            tools.addComponent(layout, ((Draggable) comp).getLayout(), column, row);
+            // Add component alignment if given
+
+        }
+    }
 
 }
--- modules/web/src/com/audimex/dashboard/web/dashboard/tools/DropLayoutTools.java
+++ modules/web/src/com/audimex/dashboard/web/dashboard/tools/DropLayoutTools.java
@@ -4,16 +4,26 @@
 
 package com.audimex.dashboard.web.dashboard.tools;
 
-import com.audimex.dashboard.model.visual_model.*;
+import com.audimex.dashboard.model.visual_model.DashboardLayout;
+import com.audimex.dashboard.model.visual_model.GridLayout;
+import com.audimex.dashboard.model.visual_model.HorizontalLayout;
+import com.audimex.dashboard.model.visual_model.VerticalLayout;
+import com.audimex.dashboard.web.dashboard.drop_handlers.GridLayoutDropHandler;
 import com.audimex.dashboard.web.dashboard.drop_handlers.HorizontalLayoutDropHandler;
-import com.audimex.dashboard.web.dashboard.drop_handlers.NotDropHandler;
 import com.audimex.dashboard.web.dashboard.drop_handlers.VerticalLayoutDropHandler;
 import com.audimex.dashboard.web.dashboard.frames.canvas.CanvasFrame;
 import com.audimex.dashboard.web.dashboard.frames.grid_creation_dialog.GridCreationDialog;
 import com.audimex.dashboard.web.dashboard.layouts.*;
+import com.vaadin.ui.AbstractLayout;
 import com.vaadin.ui.AbstractOrderedLayout;
+import fi.jasoft.dragdroplayouts.DDGridLayout;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static com.haulmont.cuba.gui.WindowManager.OpenType.DIALOG;
+import static java.util.Collections.emptyList;
+import static java.util.Collections.singletonList;
 
 @org.springframework.stereotype.Component
 public class DropLayoutTools {
@@ -25,11 +35,22 @@ public class DropLayoutTools {
         addDropHandler(rootContainer);
     }
 
-    public void addComponent(AbstractOrderedLayout targetLayout, DashboardLayout layout) {
-        addComponent(targetLayout, layout, null);
+    public void addComponent(AbstractLayout target, DashboardLayout layout) {
+        addComponent(target, layout, emptyList());
     }
 
-    public void addComponent(AbstractOrderedLayout target, DashboardLayout layout, Integer indexTo) {
+    public void addComponent(AbstractLayout target, DashboardLayout layout, Integer indexTo) {
+        addComponent(target, layout, singletonList(indexTo));
+    }
+
+    public void addComponent(AbstractLayout target, DashboardLayout layout, Integer column, Integer row) {
+        addComponent(target, layout, new ArrayList<Object>() {{
+            add(column);
+            add(row);
+        }});
+    }
+
+    protected void addComponent(AbstractLayout target, DashboardLayout layout, List<Object> args) {
         if (layout instanceof GridLayout) {
             GridCreationDialog dialog = (GridCreationDialog) targetFrame.openWindow(GridCreationDialog.SCREEN_NAME, DIALOG);
             dialog.addCloseListener(actionId -> {
@@ -48,7 +69,7 @@ public class DropLayoutTools {
                     }
 
                     addDropHandler(canvasLayout);
-                    addCanvasLayout(canvasLayout, target, indexTo);
+                    addCanvasLayout(canvasLayout, target, args);
                 }
             });
         } else {
@@ -62,17 +83,18 @@ public class DropLayoutTools {
 
             if (canvasLayout != null) {
                 addDropHandler(canvasLayout);
-                addCanvasLayout(canvasLayout, target, indexTo);
+                addCanvasLayout(canvasLayout, target, args);
             }
         }
-
     }
 
-    protected void addCanvasLayout(CanvasLayout canvasLayout, AbstractOrderedLayout target, Integer indexTo) {
-        if (indexTo == null) {
+    protected void addCanvasLayout(CanvasLayout canvasLayout, AbstractLayout target, List<Object> args) {
+        if (args.size() == 0) {
             target.addComponent(canvasLayout);
-        } else {
-            target.addComponent(canvasLayout, indexTo);
+        } else if (args.size() == 1) {
+            ((AbstractOrderedLayout) target).addComponent(canvasLayout, (int) args.get(0));
+        } else if (args.size() == 2) {
+            ((DDGridLayout) target).addComponent(canvasLayout, (int) args.get(0), (int) args.get(1));
         }
     }
 
@@ -82,7 +104,7 @@ public class DropLayoutTools {
         } else if (layout instanceof CanvasHorizontalLayout) {
             layout.setDropHandler(new HorizontalLayoutDropHandler(this));
         } else if (layout instanceof CanvasGridLayout) {
-            layout.setDropHandler(new NotDropHandler());
+            layout.setDropHandler(new GridLayoutDropHandler(this));
         } else if (layout instanceof CanvasWidgetLayout) {
 
         }
