caff4e5bed9a54f86351ef0e7bf784f68aea160c
Evgeniy Loginov <loginov@haulmont.com>
2018-05-25 18:51:38 +0400

    add init xml scheme for dashboard-ui component

---
+++ modules/gui/src/com/audimex/dashboard/gui/components/DashboardFrame.java
@@ -0,0 +1,9 @@
+/*
+ * Copyright (c) 2016-2018 Haulmont. All rights reserved.
+ */
+
+package com.audimex.dashboard.gui.components;
+
+public interface DashboardFrame {
+    String NAME = "amdxDashboardFrame";
+}
