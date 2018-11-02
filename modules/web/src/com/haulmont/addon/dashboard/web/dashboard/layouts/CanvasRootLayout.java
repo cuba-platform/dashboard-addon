e86ad2a7cfc7c2492853f98fffccd6ca85c66538
Evgeny Zaharchenko <zaharchenko@haulmont.com>
2018-11-02 12:26:59 +0400

    DASH-172 Support css layouts in dashboards (fix bugs)

--- modules/web/src/com/haulmont/addon/dashboard/web/dashboard/layouts/CanvasRootLayout.java
+++ modules/web/src/com/haulmont/addon/dashboard/web/dashboard/layouts/CanvasRootLayout.java
@@ -6,13 +6,19 @@ package com.haulmont.addon.dashboard.web.dashboard.layouts;
 
 import com.haulmont.addon.dashboard.model.visualmodel.RootLayout;
 import com.haulmont.addon.dnd.web.gui.components.WebDDVerticalLayout;
+import com.haulmont.cuba.core.global.AppBeans;
+import com.haulmont.cuba.gui.components.ScrollBoxLayout;
+import com.haulmont.cuba.gui.xml.layout.ComponentsFactory;
 
 public class CanvasRootLayout extends AbstractCanvasLayout {
     protected WebDDVerticalLayout verticalLayout;
 
     public CanvasRootLayout(RootLayout model) {
-        super(model, new WebDDVerticalLayout());
-        verticalLayout = (WebDDVerticalLayout) delegate;
+        super(model, AppBeans.get(ComponentsFactory.class).createComponent(ScrollBoxLayout.class));
+        ScrollBoxLayout scrollBoxLayout = (ScrollBoxLayout) delegate;
+        scrollBoxLayout.setSizeFull();
+        verticalLayout = new WebDDVerticalLayout();
+        scrollBoxLayout.add(verticalLayout);
     }
 
     @Override
