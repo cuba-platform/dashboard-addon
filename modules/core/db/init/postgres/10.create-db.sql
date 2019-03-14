c9a180fc7b8d4af6d11ead9391d0051bd374b445
zaharchenko <zaharchenko@haulmont.com>
2019-03-14 13:26:34 +0400

    Upgrade to CUBA 7.0
    update createDB scripts

--- modules/core/db/init/postgres/10.create-db.sql
+++ modules/core/db/init/postgres/10.create-db.sql
@@ -1,3 +1,54 @@
+-- begin DASHBOARD_WIDGET_TEMPLATE
+create table DASHBOARD_WIDGET_TEMPLATE (
+    ID uuid,
+    VERSION integer not null,
+    CREATE_TS timestamp,
+    CREATED_BY varchar(50),
+    UPDATE_TS timestamp,
+    UPDATED_BY varchar(50),
+    DELETE_TS timestamp,
+    DELETED_BY varchar(50),
+    --
+    NAME varchar(255) not null,
+    IS_AVAILABLE_FOR_ALL_USERS boolean,
+    WIDGET_MODEL text not null,
+    GROUP_ID uuid,
+    --
+    primary key (ID)
+)^
+-- end DASHBOARD_WIDGET_TEMPLATE
+-- begin DASHBOARD_WIDGET_TEMPLATE_GROUP
+create table DASHBOARD_WIDGET_TEMPLATE_GROUP (
+    ID uuid,
+    VERSION integer not null,
+    CREATE_TS timestamp,
+    CREATED_BY varchar(50),
+    UPDATE_TS timestamp,
+    UPDATED_BY varchar(50),
+    DELETE_TS timestamp,
+    DELETED_BY varchar(50),
+    --
+    NAME varchar(255) not null,
+    --
+    primary key (ID)
+)^
+-- end DASHBOARD_WIDGET_TEMPLATE_GROUP
+-- begin DASHBOARD_DASHBOARD_GROUP
+create table DASHBOARD_DASHBOARD_GROUP (
+    ID uuid,
+    VERSION integer not null,
+    CREATE_TS timestamp,
+    CREATED_BY varchar(50),
+    UPDATE_TS timestamp,
+    UPDATED_BY varchar(50),
+    DELETE_TS timestamp,
+    DELETED_BY varchar(50),
+    --
+    NAME varchar(255) not null,
+    --
+    primary key (ID)
+)^
+-- end DASHBOARD_DASHBOARD_GROUP
 -- begin DASHBOARD_PERSISTENT_DASHBOARD
 create table DASHBOARD_PERSISTENT_DASHBOARD (
     ID uuid,
@@ -18,54 +69,3 @@ create table DASHBOARD_PERSISTENT_DASHBOARD (
     primary key (ID)
 )^
 -- end DASHBOARD_PERSISTENT_DASHBOARD
--- begin DASHBOARD_WIDGET_TEMPLATE
-create table DASHBOARD_WIDGET_TEMPLATE (
-    ID uuid,
-    VERSION integer not null,
-    CREATE_TS timestamp,
-    CREATED_BY varchar(50),
-    UPDATE_TS timestamp,
-    UPDATED_BY varchar(50),
-    DELETE_TS timestamp,
-    DELETED_BY varchar(50),
-    --
-    NAME varchar(255) not null,
-    IS_AVAILABLE_FOR_ALL_USERS boolean,
-    WIDGET_MODEL text not null,
-    GROUP_ID uuid,
-    --
-    primary key (ID)
-)^
--- end DASHBOARD_WIDGET_TEMPLATE
--- begin DASHBOARD_DASHBOARD_GROUP
-create table DASHBOARD_DASHBOARD_GROUP (
-    ID uuid,
-    VERSION integer not null,
-    CREATE_TS timestamp,
-    CREATED_BY varchar(50),
-    UPDATE_TS timestamp,
-    UPDATED_BY varchar(50),
-    DELETE_TS timestamp,
-    DELETED_BY varchar(50),
-    --
-    NAME varchar(255) not null,
-    --
-    primary key (ID)
-)^
--- end DASHBOARD_DASHBOARD_GROUP
--- begin DASHBOARD_WIDGET_TEMPLATE_GROUP
-create table DASHBOARD_WIDGET_TEMPLATE_GROUP (
-    ID uuid,
-    VERSION integer not null,
-    CREATE_TS timestamp,
-    CREATED_BY varchar(50),
-    UPDATE_TS timestamp,
-    UPDATED_BY varchar(50),
-    DELETE_TS timestamp,
-    DELETED_BY varchar(50),
-    --
-    NAME varchar(255) not null,
-    --
-    primary key (ID)
-)^
--- end DASHBOARD_WIDGET_TEMPLATE_GROUP
